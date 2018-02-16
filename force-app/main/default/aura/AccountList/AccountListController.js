3b6c7b2f1ca2099954588d5067a4773cfb13430a
Bobby White <bobby.white@salesforce.com>
2018-02-15 22:57:54 -0500

    Exercise 2 checkpoint2

---
+++ force-app/main/default/aura/AccountList/AccountListController.js
@@ -0,0 +1,10 @@
+({
+    doInit : function(component, event) {
+        var action = component.get("c.findAll");
+        action.setCallback(this, function(a) {
+          component.set("v.accounts", a.getReturnValue()); 
+        });
+        $A.enqueueAction(action);
+    }
+})
+
